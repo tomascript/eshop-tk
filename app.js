01108a31a2de68dc4b65d913034e98983c20b2e5
tomascript <tomas.ko@centrum.sk>
2020-08-31 22:58:21 +0200

    file unique name update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -24,7 +25,7 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(null, uuidv4() + '-' + file.originalname);
   }
 });
 
